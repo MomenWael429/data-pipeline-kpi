f67575e67eaa39ff11f22952eab968c727148bc8
m.badawy <m.badawy@fasttube.de>
2026-05-04 19:09:14 +0200

    fixes

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def create_chart(df):
-    df["timestamp:"] = pd.to_datetime(df["timestamp"])
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
 
     plt.figure()
     plt.plot(df["timestamp"], df["price"])
